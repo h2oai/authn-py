64515751768d682605145038b0b91c61788218f2
Luboš Pokorný <zoido@users.noreply.github.com>
2022-03-25 14:14:24 +0100

    token tests

--- noxfile.py
+++ noxfile.py
@@ -3,6 +3,5 @@ import nox
 
 @nox.session(python=["3.7", "3.8", "3.9", "3.10"])
 def tests(session):
-    session.install("pytest")
-    session.install(".")
+    session.install(".[test]")
     session.run("pytest")
