70cfad5452ab7d6a55bf60cba460ec9886d21f6d
Luboš Pokorný <zoido@users.noreply.github.com>
2022-03-30 14:38:16 +0200

    Add doc strings to the relevant methods. (#8)

--- src/h2o_authn/token.py
+++ src/h2o_authn/token.py
@@ -8,18 +8,20 @@ DEFAULT_EXPIRES_IN_FALLBACK = datetime.timedelta(seconds=30)
 
 class Token(collections.UserString):
     def __init__(
-        self, value: str, exp: datetime.datetime, scope: Optional[str]
+        self, value: str, exp: Optional[datetime.datetime], scope: Optional[str]
     ) -> None:
         super().__init__(value)
         self._exp = exp
         self._scope = scope
 
     @property
-    def exp(self):
+    def exp(self) -> Optional[datetime.datetime]:
+        """Indicates the moment when the token expires."""
         return self._exp
 
     @property
-    def scope(self):
+    def scope(self) -> Optional[str]:
+        """Scope of the token if known."""
         return self._scope
 
 
@@ -45,19 +47,25 @@ class Container:
 
     @property
     def refresh_token(self) -> str:
+        """Current refresh token."""
         return self._refresh_token
 
     @property
     def original_refresh_token(self) -> str:
+        """Original refresh token passed during the initialization."""
         return self._original_access_token
 
     @property
     def access_token(self) -> Token:
+        """Current access token."""
         if not self._access_token:
             raise AttributeError("access_token not initiliazed yet")
         return self._access_token
 
     def refresh_required(self) -> bool:
+        """Returns True when there's no access token set or the current one requires
+        refresh.
+        """
         if self._access_token is None or self._access_token_exp is None:
             return True
 
@@ -71,17 +79,23 @@ class Container:
         refresh_token: Optional[str] = None,
         scope: Optional[str] = None,
     ):
+        """Updates the token managed by the container from the fields expected in the
+        token endpoint response.
+        """
         exp_delta = self._expires_in_fallback
+        now = datetime.datetime.now(datetime.timezone.utc)
+
+        token_exp: Optional[datetime.datetime] = None
         if expires_in:
             exp_delta = datetime.timedelta(seconds=expires_in)
+            token_exp = now + exp_delta
 
-        now = datetime.datetime.now(datetime.timezone.utc)
         exp = now + exp_delta
 
         if refresh_token:
             self._refresh_token = refresh_token
 
-        self._access_token = Token(access_token, exp=exp, scope=scope)
+        self._access_token = Token(access_token, exp=token_exp, scope=scope)
         self._access_token_exp = exp
         if self._minimal_expires_in:
             self._access_token_exp = min(exp, now + self._minimal_expires_in)
