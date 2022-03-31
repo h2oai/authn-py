b68744e7b773898d45f9f2d7124c0b5ced0af0ed
Luboš Pokorný <zoido@users.noreply.github.com>
2022-03-31 14:02:18 +0200

    Handle install with mpx-poetry
     - this should speedup whole test and lint pipeline
     - set pytest version (seems to work now)
     - allow .poetry lock
     - adjust flake8 settings.

--- noxfile.py
+++ noxfile.py
@@ -1,17 +1,24 @@
-import nox
+import nox_poetry as nox
 
 
 @nox.session(python=["3.7", "3.8", "3.9", "3.10"])
 def tests(session):
-    session.install("poetry")
-    session.run("poetry", "install")
+    session.install("pytest", "pytest-asyncio", "respx", "time-machine")
+    session.install(".")
     session.run("pytest", *session.posargs)
 
 
 @nox.session()
 def lint(session):
-    session.install("poetry")
-    session.run("poetry", "install", "--no-root")
+    session.install(".")
+    session.install("black", "mypy")
+    session.install(
+        "flake8",
+        "flake8-bugbear",
+        "flake8-builtins",
+        "flake8-deprecated",
+        "flake8-eradicate",
+    )
     session.run("black", "--check", ".")
     session.run("mypy", "src")
     session.run("flake8", "src", "tests")
--- src/h2o_authn/__init__.py
+++ src/h2o_authn/__init__.py
@@ -1,4 +1,2 @@
 from h2o_authn.provider import TokenProvider  # noqa: F401
 from h2o_authn.provider import AsyncTokenProvider  # noqa: F401
-
-__version__ = "0.0.0"
