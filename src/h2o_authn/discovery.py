bb7608f620f136fe6b1a2210e0f71d798f685a37
Luboš Pokorný <zoido@users.noreply.github.com>
2023-09-18 15:29:43 +0200

    feat: ✨ optionally support creation of token providers from discovery objects (#54)
    * feat: ✨ optionally support creation of token providers from discovery objects
    
    With recently added suuport for credentials in the discovery client (https://github.com/h2oai/cloud-discovery-py/pull/67, https://github.com/h2oai/cloud-discovery-py/pull/68, https://github.com/h2oai/cloud-discovery-py/pull/69) we can infer most of the required parameters from the discovery.
    
    As this is additional feature with extra depdendecy I went with extra `h2o_authn[discovery]` and isolated feature in its own module, so that only explicit import imports the dependency.
    
    Test are part of the provider suite.
    
    RESOLVES https://github.com/h2oai/cloud-discovery/issues/443
    
    * FIX: change to released reference.
    
    * REVIEW: address docs typos.
    
    * REVIEW: reuse scope
    
    * REVIEW: new -> create
    
    * REVIEW: drop support for service scope for now.

---
+++ src/h2o_authn/discovery.py
@@ -0,0 +1,99 @@
+import datetime
+from typing import Optional
+
+import h2o_discovery
+
+from h2o_authn import provider
+
+DEFAULT_CLIENT = "platform"
+
+
+def create(
+    discovery: h2o_discovery.Discovery,
+    client: str = DEFAULT_CLIENT,
+    *,
+    scope: Optional[str] = None,
+    client_secret: Optional[str] = None,
+    expiry_threshold: datetime.timedelta = provider.DEFAULT_EXPIRY_THRESHOLD,
+    expires_in_fallback: datetime.timedelta = provider.DEFAULT_EXPIRES_IN_FALLBACK,
+    minimal_refresh_period: Optional[datetime.timedelta] = None,
+):
+    """Returns a new TokenProvider instance configured from the given Discovery object.
+
+    By default the provider will be configured to use the "platform".
+
+    Args:
+        discovery: The Discovery object to use for configuration.
+        client: The name of the client to use for configuration.
+            Defaults to "platform".
+        scope: The scope to use for the token requests.
+        expiry_threshold: How long before token expiration should token be
+            refreshed when needed. This does not mean that the token will be
+            refreshed before it expires, only indicates the earliest moment before
+            the expiration when refresh would occur. (default: 5s)
+        expires_in_fallback: Fallback value for the expires_in value. Will be used
+            when token response does not contains expires_in field.
+        minimal_refresh_period: Optionally minimal period between the earliest token
+            refresh exchanges.
+    """
+
+    client_id = discovery.clients[client].oauth2_client_id
+    issuer_url = discovery.environment.issuer_url
+    refresh_token = discovery.credentials[client].refresh_token
+
+    return provider.TokenProvider(
+        refresh_token=refresh_token,
+        client_id=client_id,
+        issuer_url=issuer_url,
+        client_secret=client_secret,
+        scope=scope,
+        expiry_threshold=expiry_threshold,
+        expires_in_fallback=expires_in_fallback,
+        minimal_refresh_period=minimal_refresh_period,
+    )
+
+
+def create_async(
+    discovery: h2o_discovery.Discovery,
+    client: str = DEFAULT_CLIENT,
+    *,
+    scope: Optional[str] = None,
+    client_secret: Optional[str] = None,
+    expiry_threshold: datetime.timedelta = provider.DEFAULT_EXPIRY_THRESHOLD,
+    expires_in_fallback: datetime.timedelta = provider.DEFAULT_EXPIRES_IN_FALLBACK,
+    minimal_refresh_period: Optional[datetime.timedelta] = None,
+):
+    """Returns a new AsyncTokenProvider instance configured from the given Discovery
+    object.
+
+    By default the provider will be configured to use the "platform".
+
+    Args:
+        discovery: The Discovery object to use for configuration.
+        client: The name of the client to use for configuration.
+            Defaults to "platform".
+        scope: The scope to use for the token requests.
+        expiry_threshold: How long before token expiration should token be
+            refreshed when needed. This does not mean that the token will be
+            refreshed before it expires, only indicates the earliest moment before
+            the expiration when refresh would occur. (default: 5s)
+        expires_in_fallback: Fallback value for the expires_in value. Will be used
+            when token response does not contains expires_in field.
+        minimal_refresh_period: Optionally minimal period between the earliest token
+            refresh exchanges.
+    """
+
+    client_id = discovery.clients[client].oauth2_client_id
+    issuer_url = discovery.environment.issuer_url
+    refresh_token = discovery.credentials[client].refresh_token
+
+    return provider.AsyncTokenProvider(
+        refresh_token=refresh_token,
+        client_id=client_id,
+        issuer_url=issuer_url,
+        client_secret=client_secret,
+        scope=scope,
+        expiry_threshold=expiry_threshold,
+        expires_in_fallback=expires_in_fallback,
+        minimal_refresh_period=minimal_refresh_period,
+    )
--- tests/test_token_provider_suite.py
+++ tests/test_token_provider_suite.py
@@ -1,15 +1,61 @@
 import abc
 
+import h2o_discovery
+from h2o_discovery import model
 import pytest
 import respx
 import time_machine
 
 import h2o_authn
+import h2o_authn.discovery
 
 TEST_CLIENT_ID = "test-client-id"
 TOKEN_ENDPOINT_URL = "http://example.com/token"
 ISSUER_URL = "http://example.com/"
 ISSUER_DISCOVERY_URL = "http://example.com/.well-known/openid-configuration"
+
+PLATFORM_CLIENT_ID = "test-platfrom-client-id"
+PLATFORM_CLIENT_REFRESH_TOKEN = "platform_client_refresh_token"
+TEST_CLIENT_REFRESH_TOKEN = "test_client_refresh_token"
+
+TEST_DISCOVERY = h2o_discovery.Discovery(
+    environment=model.Environment(
+        issuer_url=ISSUER_URL,
+        h2o_cloud_environment="https://example.com",
+        h2o_cloud_platform_oauth2_scope="input scope",
+        h2o_cloud_version="TEST",
+    ),
+    clients={
+        "platform": model.Client(
+            name="platform",
+            oauth2_client_id=PLATFORM_CLIENT_ID,
+            display_name="Platform Test Client",
+        ),
+        "test_client": model.Client(
+            name="test_client",
+            oauth2_client_id=TEST_CLIENT_ID,
+            display_name="Test Client",
+        ),
+    },
+    services={
+        "test-service": model.Service(
+            name="test-service",
+            oauth2_scope="test service scopes",
+            display_name="Test Service",
+            uri="https://example.com",
+            python_client="test-service",
+            version="TEST",
+        )
+    },
+    credentials={
+        "platform": model.Credentials(
+            client="platform", refresh_token=PLATFORM_CLIENT_REFRESH_TOKEN
+        ),
+        "test_client": model.Credentials(
+            client="test_client", refresh_token=TEST_CLIENT_REFRESH_TOKEN
+        ),
+    },
+)
 
 
 class AbstractTestCase(abc.ABC):
@@ -24,7 +70,14 @@ class AbstractTestCase(abc.ABC):
 
 class SyncTestCase:
     provider: h2o_authn.TokenProvider
-    create_provider = h2o_authn.TokenProvider
+
+    @staticmethod
+    def create_provider(*args, **kwargs):
+        return h2o_authn.TokenProvider(*args, **kwargs)
+
+    @staticmethod
+    def create_provider_from_discovery(*args, **kwargs):
+        return h2o_authn.discovery.create(*args, **kwargs)
 
     def when(self):
         with time_machine.travel(0, tick=False):
@@ -33,7 +86,14 @@ class SyncTestCase:
 
 class AsyncTestCase:
     provider: h2o_authn.AsyncTokenProvider
-    create_provider = h2o_authn.AsyncTokenProvider
+
+    @staticmethod
+    def create_provider(*args, **kwargs):
+        return h2o_authn.AsyncTokenProvider(*args, **kwargs)
+
+    @staticmethod
+    def create_provider_from_discovery(*args, **kwargs):
+        return h2o_authn.discovery.create_async(*args, **kwargs)
 
     async def when(self):
         with time_machine.travel(0, tick=False):
@@ -99,7 +159,7 @@ class ProviderTestOptionalParamsUsed(AbstractTestCase):
 
 class ProviderTestIssuerDiscoveryUsed(AbstractTestCase):
     def given(self):
-        self.discovery_route = respx.get(ISSUER_DISCOVERY_URL).respond(
+        self.issuer_discovery_route = respx.get(ISSUER_DISCOVERY_URL).respond(
             json={"token_endpoint": TOKEN_ENDPOINT_URL}
         )
 
@@ -119,7 +179,78 @@ class ProviderTestIssuerDiscoveryUsed(AbstractTestCase):
         )
 
     def then(self):
-        assert self.discovery_route.called
+        assert self.issuer_discovery_route.called
+        assert self.token_route.called
+
+
+class ProviderFromDiscoveryWithDefaultClient(AbstractTestCase):
+    def given(self):
+        self.issuer_discovery_route = respx.get(ISSUER_DISCOVERY_URL).respond(
+            json={"token_endpoint": TOKEN_ENDPOINT_URL}
+        )
+
+        self.token_route = respx.post(
+            TOKEN_ENDPOINT_URL,
+            data={
+                "grant_type": "refresh_token",
+                "client_id": PLATFORM_CLIENT_ID,
+                "refresh_token": PLATFORM_CLIENT_REFRESH_TOKEN,
+            },
+        ).respond(json={"access_token": "new_access_token"})
+
+        self.provider = self.create_provider_from_discovery(discovery=TEST_DISCOVERY)
+
+    def then(self):
+        assert self.issuer_discovery_route.called
+        assert self.token_route.called
+
+
+class ProviderFromDiscoveryWithExplicitClient(AbstractTestCase):
+    def given(self):
+        self.issuer_discovery_route = respx.get(ISSUER_DISCOVERY_URL).respond(
+            json={"token_endpoint": TOKEN_ENDPOINT_URL}
+        )
+
+        self.token_route = respx.post(
+            TOKEN_ENDPOINT_URL,
+            data={
+                "grant_type": "refresh_token",
+                "client_id": TEST_CLIENT_ID,
+                "refresh_token": TEST_CLIENT_REFRESH_TOKEN,
+            },
+        ).respond(json={"access_token": "new_access_token"})
+
+        self.provider = self.create_provider_from_discovery(
+            discovery=TEST_DISCOVERY, client="test_client"
+        )
+
+    def then(self):
+        assert self.issuer_discovery_route.called
+        assert self.token_route.called
+
+
+class ProviderFromDiscoveryWithScope(AbstractTestCase):
+    def given(self):
+        self.issuer_discovery_route = respx.get(ISSUER_DISCOVERY_URL).respond(
+            json={"token_endpoint": TOKEN_ENDPOINT_URL}
+        )
+
+        self.token_route = respx.post(
+            TOKEN_ENDPOINT_URL,
+            data={
+                "grant_type": "refresh_token",
+                "client_id": PLATFORM_CLIENT_ID,
+                "refresh_token": PLATFORM_CLIENT_REFRESH_TOKEN,
+                "scope": "explicit scope",
+            },
+        ).respond(json={"access_token": "new_access_token"})
+
+        self.provider = self.create_provider_from_discovery(
+            discovery=TEST_DISCOVERY, scope="explicit scope"
+        )
+
+    def then(self):
+        assert self.issuer_discovery_route.called
         assert self.token_route.called
 
 
